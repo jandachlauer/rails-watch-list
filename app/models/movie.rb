292aebd6a979c5870cd9ab3652745c7c4d5e5227
Jan Dachlauer <jandachlauer@gmail.com>
2023-02-23 12:43:58 +0100

    Models and validations and assocations done

---
+++ app/models/movie.rb
@@ -0,0 +1,6 @@
+class Movie < ApplicationRecord
+  has_many :bookmarks
+  has_many :lists, through: :bookmarks
+  validates :title, :overview, presence: true, uniqueness: true, allow_blank: false
+  
+end
