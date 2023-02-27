f7633b4d17a572e32d18a8ac30e11772542e88a9
Jan Dachlauer <jandachlauer@gmail.com>
2023-02-27 14:52:04 +0100

    Delete bug and JavaScript fixed

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
 root to: "lists#index"
 
 resources :lists, only: [:create, :new, :index, :show] do
-  resources :bookmarks, only: [:new, :create, :destroy]
+  resources :bookmarks, only: [:new, :create]
 end
+resources :bookmarks, only: :destroy
 resources :movies, only: [:index, :show]
 end
